4dec74ce60997c327abbbb9bee2b520f6ad8574c
iomeanslife <omar@ajerray.de>
2025-10-07 09:32:28 +0100

    Basic user creation and login functionality

--- backend_functions.py
+++ backend_functions.py
@@ -6,13 +6,37 @@ class BackendFunctions:
         self.database = database
 
     def create_user(self, username, hash):
-        connection = sqlite3.connect(self.database)
-        cursor = connection.cursor()
-        
-        check_exists = cursor.execute(f"SELECT username FROM user WHERE username = '{username}' ") 
-        if check_exists.fetchone() is not None:
-            raise ValueError("username already exists.")
+        try:
+            connection = sqlite3.connect(self.database)
+            cursor = connection.cursor()
+            
+            check_exists = cursor.execute(f"SELECT username FROM user WHERE username = '{username}' ") 
+            if check_exists.fetchone() is not None:
+                raise ValueError("Username already exists.")
 
-        cursor.execute(f"INSERT INTO user VALUES('{username}', '{hash}') ") # TODO: check if user exists already.
-        connection.commit()
-        connection.close()
+            cursor.execute(f"INSERT INTO user VALUES('{username}', '{hash}') ")
+            connection.commit()
+
+            check_exists = cursor.execute(f"SELECT username FROM user WHERE username = '{username}' ") 
+            if check_exists.fetchone() is None:
+                raise SystemError("Creating user failed.")
+
+            return True
+        except:
+            return False
+        finally:
+            connection.close()
+
+    def login_user(self, username, hash):
+        try:
+            connection = sqlite3.connect(self.database)
+            cursor = connection.cursor()
+            
+            check_exists = cursor.execute(f"SELECT username,password_hash FROM user WHERE username = '{username}' AND password_hash = '{hash}'") 
+            if check_exists.fetchone() is None:
+                raise ValueError("wrong password.")
+            return True
+        except:
+            return False
+        finally:
+            connection.close()
--- main.py
+++ main.py
@@ -3,28 +3,105 @@ import backend_functions
 import configparser
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+backendFunctions = None
+
 class Handler(BaseHTTPRequestHandler):
+    backendFunctions = None
+
     def _set_headers(self):
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
-    
+
     def do_GET(self):
+        print(self.headers.get('Cookie'))
         self._set_headers()
-        self.wfile.write(b"Received GET request")
+
+        if self.path == "/create_user":
+            self.wfile.write(b"""<!DOCTYPE html>
+<html>
+<body>
+<h2>Login Form</h2>
+<form action="/create_user" method="POST">
+  <label for="username">Username:</label><br>
+  <input type="text" id="username" name="username" value=""><br>
+  <label for="password">Password:</label><br>
+  <input type="password" id="password" name="password" value=""><br>
+  <label for="repeat_password">Repeat Password:</label><br>
+  <input type="password" id="password" name="repeat_password" value=""><br>
+  <input type="submit" value="CreateUser">
+</form> 
+</body>
+</html>""")
+
+        else:
+            self.wfile.write(b"""<!DOCTYPE html>
+<html>
+<body>
+<h2>Login Form</h2>
+<form action="/login" method="POST">
+  <label for="username">Username:</label><br>
+  <input type="text" id="username" name="username" value=""><br>
+  <label for="password">Password:</label><br>
+  <input type="password" id="password" name="password" value=""><br>
+  <input type="submit" value="Login">
+  <a href="create_user" title="Create User">Create User</a>
+</form> 
+</body>
+</html>""")
 
     def do_POST(self):
-        self._set_headers()
         content_length = int(self.headers['Content-length'])
         post_data = self.rfile.read(content_length)
-        self.wfile.write(f"Recieved POST request: {post_data.decode('utf-8')}".encode('utf-8'))
-    
+        decoded = post_data.decode('utf-8').split('&')
+        
+        try:
+            username = decoded[0][9:]
+            hash = decoded[1][9:]
+            if len(decoded) >= 3:
+                repeat_hash = decoded[2][16:]
+        
+        except:
+            self.send_response(422)
+            self.send_header('Content-type','text/html')
+            self.end_headers()
+            self.wfile.write(f"Processing Request Failed.".encode('utf-8'))
+            return
+  
+        if self.path == "/create_user":
+            if hash != repeat_hash or Handler.backendFunctions.create_user(username, hash) is not True:
+                self.send_response(401)
+                self.send_header('Content-type','text/html')
+                self.end_headers()
+                self.wfile.write(f"Signup Failed.".encode('utf-8'))                                                                   
+                return
+            self.send_response(200)
+            self.send_header('Content-type','text/html')
+            self.send_header('Set-Cookie',f'simple_token={username}; Max-Age=5560' ) # TODO: Generate some UUID and store it in the database which the cookie can be compared to.
+            self.end_headers()
+
+            self.wfile.write(f'User "{username}" was created'.encode('utf-8'))
+            return
+        elif self.path == "/login" and Handler.backendFunctions.login_user(username, hash) is not True:
+            self.send_response(401)
+            self.send_header('Content-type','text/html')
+            self.end_headers()
+            self.wfile.write(f"Login Failed.".encode('utf-8'))
+            return
+
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.send_header('Set-Cookie',f'simple_token={username}; Max-Age=5560' )
+        self.end_headers()
+
+        self.wfile.write(f" POST request: {post_data.decode('utf-8')}".encode('utf-8'))
+
 def run(server_class=HTTPServer, handler_class=Handler, port=8000):
     config = configparser.ConfigParser()
     config.read("config.ini")
     database = config.get("Connection","Database")
     setup_database.setup_database(database)
-    backendFunctions = backend_functions.BackendFunctions(database)
+    Handler.backendFunctions = backend_functions.BackendFunctions(database)
 
     server_address = ('', port)
     httpd = server_class(server_address,handler_class)
